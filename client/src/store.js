24e3325eaea9e3d6365f86cbb4a8461d82c7ef4f
uop789 <zh901008@gmail.com>
2019-04-10 21:54:24 -0400

    change composeEnhancers

--- client/src/store.js
+++ client/src/store.js
@@ -8,10 +8,11 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(...middleWare))
+);
 
 export default store;
