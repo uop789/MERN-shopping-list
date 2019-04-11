c60ae6ea982effdef7c5cc94227ac5f8532b1bb5
uop789 <zh901008@gmail.com>
2019-04-10 22:21:23 -0400

    Version 1.0.0

--- client/src/store.js
+++ client/src/store.js
@@ -6,7 +6,7 @@ import rootReducer from './reducers';
 const initialState = {};
 
 
-const middleware = [thunk];
+const middleWare = [thunk];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
